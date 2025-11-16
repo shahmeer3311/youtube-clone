b42f4b62ce4f809aeeb6e869b1c339e60ccaca8b
app <app@apps-MacBook-Pro.local>
2025-11-16 15:27:47 +0500

    Stop tracking data.js

--- src/data.js
+++
@@ -1,8 +0,0 @@
-export const API_KEY="AIzaSyAW8966MCaxKDV5GYM00_TXlwXXoXlNCck"
-
-export const views_Converter=(views)=>{
-   return new Intl.NumberFormat('en',{
-    notation: 'compact',
-    maximumFractionDigits: 1
-   }).format(views);
-}
